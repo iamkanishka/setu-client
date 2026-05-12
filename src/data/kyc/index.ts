0bc7cee457a00086ac907b92eb2d84ade7e4faec
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2026-05-12 20:47:02 +0530

    index

---
+++ src/data/kyc/index.ts
@@ -0,0 +1,6 @@
+export * as PAN from "./pan.js";
+export * as BAV from "./bav.js";
+export * as GST from "./gst.js";
+export * as NameMatch from "./namematch.js";
+export * as EKYC from "./ekyc.js";
+export * as DigiLocker from "./digilocker.js";
